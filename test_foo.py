724c2d4c90398546c099c669dd65ea2af4997ae7
Jeff Wright <jeff.washcloth@gmail.com>
2015-01-23 08:25:25 -0500

    Extend foo test to include bar

--- test_foo.py
+++ test_foo.py
@@ -21,11 +21,11 @@ class FooTest(unittest.TestCase):
         
     def testA(self):
         '''Test routine A'''
-        print 'FooTest: testA'
+        print 'FooTest: running testA...'
         
     def testB(self):
         '''Test routine B'''
-        print 'FooTest: testB'
+        print 'FooTest: running testB...'
         
     def tearDown(self):
         '''Tear down from testing...'''
@@ -41,5 +41,46 @@ class FooTest(unittest.TestCase):
              
         print 'FooTest:tearDown_:end'
         
+class BarTest(unittest.TestCase):
+    '''Sample test case -- BarTest()'''
+    
+    def setUp(self):
+        '''Set up for testing...'''
+        print 'BarTest:setUp_:begin'
+         
+        testName = self.shortDescription()
+        if (testName == 'Test routine A'):
+            print 'setting up for test A'
+        elif (testName == 'Test routine B'):
+            print 'setting up for test B'
+        else:
+            print 'UNKNOWN TEST ROUTINE'
+             
+        print 'BarTest:setUp_:end'
+        
+    def testA(self):
+        '''Test routine A'''
+        print 'BarTest: running testA...'
+        
+    def testB(self):
+        '''Test routine B'''
+        print 'BarTest: running testB...'
+        
+    def tearDown(self):
+        '''Tear down from testing...'''
+        print 'BarTest:tearDown_:begin'
+         
+        testName = self.shortDescription()
+        if (testName == 'Test routine A'):
+            print 'cleaning up after test A'
+        elif (testName == 'Test routine B'):
+            print 'cleaning up after test B'
+        else:
+            print 'UNKNOWN TEST ROUTINE'
+             
+        print 'BarTest:tearDown_:end'
+        
+
+
 if __name__ == '__main__':
     unittest.main()
