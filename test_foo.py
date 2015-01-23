bb42b3665d28df4dfb1efc8fc134777ad83c3ef3
Jeff Wright <jeff.washcloth@gmail.com>
2015-01-22 21:39:46 -0500

    Add specific routines for Test A and Test B in setUp() and tearDown()

--- test_foo.py
+++ test_foo.py
@@ -7,19 +7,39 @@ class FooTest(unittest.TestCase):
     
     def setUp(self):
         '''Set up for testing...'''
-        print 'FooTest: setUp()'
+        print 'FooTest:setUp_:begin'
+         
+        testName = self.shortDescription()
+        if (testName == 'Test routine A'):
+            print 'setting up for test A'
+        elif (testName == 'Test routine B'):
+            print 'setting up for test B'
+        else:
+            print 'UNKNOWN TEST ROUTINE'
+             
+        print 'FooTest:setUp_:end'
         
     def testA(self):
-        '''Test routine A...'''
+        '''Test routine A'''
         print 'FooTest: testA'
         
     def testB(self):
-        '''Test routine B...'''
+        '''Test routine B'''
         print 'FooTest: testB'
         
-    def tearDown(sefl):
+    def tearDown(self):
         '''Tear down from testing...'''
-        print 'FooTest: tearDown()'
+        print 'FooTest:tearDown_:begin'
+         
+        testName = self.shortDescription()
+        if (testName == 'Test routine A'):
+            print 'cleaning up after test A'
+        elif (testName == 'Test routine B'):
+            print 'cleaning up after test B'
+        else:
+            print 'UNKNOWN TEST ROUTINE'
+             
+        print 'FooTest:tearDown_:end'
         
 if __name__ == '__main__':
     unittest.main()
